7bbdac99b25c8de95289426e3c28abc02e4d8b60
Nguyen Quoc Huy <150227535+huy-dataguy@users.noreply.github.com>
2023-11-23 15:35:48 +0700

    Update FunctionDic.js

--- FunctionDic.js
+++ FunctionDic.js
@@ -1,1 +1,10 @@
-
+const dictionary = new Dictionary();
+const ielts = new Dictionary();
+dictionary.init("word_define.txt");
+document.getElementById("searchButton").addEventListener("click", display);
+document.getElementById("input").addEventListener("keyup", function (event) {
+    if (event.key == "Enter")
+        display();
+});
+document.getElementById("input").addEventListener("input", handleInput);
+document.getElementById("insertButton").addEventListener("click", insertVocab);
